0f452dc948896633dea4f24022fc8a3c72b81915
KarnesTH <p_haehnel@hotmail.de>
2025-05-02 19:39:02 +0200

    edit: add helper functions

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,6 @@
-use inquire::{Confirm, Text};
+use inquire::{Confirm, Select, Text};
 
-use crate::config::LocalConfig;
+use crate::config::{LocalConfig, ServerConnectionType};
 
 pub struct InitialCommand {}
 
@@ -20,13 +20,7 @@ impl InitialCommand {
 
         if config.is_initialized {
             println!("Configure your server");
-            if let Some(name) = name {
-                print!("Name: {}", name);
-            } else {
-                Text::new("Enter a name for your server")
-                    .with_help_message("The name of your Server e.g. DayZFrostlineServer")
-                    .prompt()?;
-            }
+            let name = Self::get_name(name)?;
         } else {
             println!("Initialize config");
             if let Some(name) = name {
@@ -39,5 +33,119 @@ impl InitialCommand {
         }
 
         Ok(())
+    }
+
+    fn init_config() -> Result<(), Box<dyn std::error::Error>> {
+        todo!("Implement initialize functionality");
+    }
+
+    fn get_name(name: Option<String>) -> Result<String, Box<dyn std::error::Error>> {
+        if let Some(name) = name {
+            Ok(name)
+        } else {
+            Ok(Text::new("Enter a name for your server")
+                .with_help_message("The name of your Server e.g. DayZFrostlineServer")
+                .prompt()?)
+        }
+    }
+
+    fn get_connection_type(
+        connection_type: Option<String>,
+    ) -> Result<ServerConnectionType, Box<dyn std::error::Error>> {
+        if let Some(connection_type) = connection_type {
+            match connection_type.as_str() {
+                "local" => Ok(ServerConnectionType::Local),
+                "ftp" => Ok(ServerConnectionType::Ftp),
+                "sftp" => Ok(ServerConnectionType::Sftp),
+                _ => Err("Invalid connection type".into()),
+            }
+        } else {
+            let connection_types = vec!["local", "ftp", "sftp"];
+            let selected_type = Select::new("Select a connection type", connection_types)
+                .with_help_message("The type of connection to use")
+                .prompt()?;
+            match selected_type {
+                "local" => Ok(ServerConnectionType::Local),
+                "ftp" => Ok(ServerConnectionType::Ftp),
+                "sftp" => Ok(ServerConnectionType::Sftp),
+                _ => Err("Invalid connection type".into()),
+            }
+        }
+    }
+
+    fn get_steamcmd_path(
+        steamcmd_path: Option<String>,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        if let Some(path) = steamcmd_path {
+            Ok(path)
+        } else {
+            Ok(Text::new("Enter the path to your SteamCMD installation")
+                .with_help_message("The path to your SteamCMD installation")
+                .prompt()?)
+        }
+    }
+
+    fn get_workshop_path(
+        workshop_path: Option<String>,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        if let Some(path) = workshop_path {
+            Ok(path)
+        } else {
+            Ok(Text::new("Enter the path to your workshop directory")
+                .with_help_message("The path to your workshop directory")
+                .prompt()?)
+        }
+    }
+
+    fn get_server_path(server_path: Option<String>) -> Result<String, Box<dyn std::error::Error>> {
+        if let Some(path) = server_path {
+            Ok(path)
+        } else {
+            Ok(Text::new("Enter the path to your server directory")
+                .with_help_message("The path to your server directory")
+                .prompt()?)
+        }
+    }
+
+    fn get_hostname(hostname: Option<String>) -> Result<String, Box<dyn std::error::Error>> {
+        if let Some(hostname) = hostname {
+            Ok(hostname)
+        } else {
+            Ok(Text::new("Enter the hostname of your FTP server")
+                .with_help_message("The hostname of your FTP server")
+                .prompt()?)
+        }
+    }
+
+    fn get_username(username: Option<String>) -> Result<String, Box<dyn std::error::Error>> {
+        if let Some(username) = username {
+            Ok(username)
+        } else {
+            Ok(Text::new("Enter the username for your FTP server")
+                .with_help_message("The username for your FTP server")
+                .prompt()?)
+        }
+    }
+
+    fn get_password(password: Option<String>) -> Result<String, Box<dyn std::error::Error>> {
+        if let Some(password) = password {
+            Ok(password)
+        } else {
+            Ok(Text::new("Enter the password for your FTP server")
+                .with_help_message("The password for your FTP server")
+                .prompt()?)
+        }
+    }
+
+    fn get_port(port: Option<String>) -> Result<u16, Box<dyn std::error::Error>> {
+        if let Some(port) = port {
+            port.parse().map_err(|_| "Invalid port number".into())
+        } else {
+            Ok(Text::new("Enter the port number for your FTP server")
+                .with_help_message("The port number for your FTP server")
+                .prompt()?
+                .parse()
+                .map_err(|_| "Invalid port number".to_string())?)
+        }
     }
 }
